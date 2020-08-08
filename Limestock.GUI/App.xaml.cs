6194a9daba6eaeabcb450d26511cb9156d58515e
LemonSprite <lemonsprite@github.com>
2020-08-08 18:57:09 +0700

    bikin login window aplikasi

---
+++ Limestock.GUI/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Limestock.GUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Window loginWindow = new LoginWindow();
+            loginWindow.Show();
+
+
+            base.OnStartup(e);
+        }
+    }
+}
