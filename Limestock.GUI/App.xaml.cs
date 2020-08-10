18e407723a447c27555ae7dac780bb5032a1e0a5
LemonSprite <lemonsprite@github.com>
2020-08-10 13:53:18 +0700

    Bikin splashscreen app

--- Limestock.GUI/App.xaml.cs
+++ Limestock.GUI/App.xaml.cs
@@ -15,23 +15,27 @@ namespace Limestock.GUI
     /// </summary>
     public partial class App : Application
     {
-        private const int DURASI = 5000;
+
+        private const int durasiMax = 5000;
         protected override void OnStartup(StartupEventArgs e)
         {
-
+            // instansiasi splashScreen
             SplashScreen splashScreen = new SplashScreen();
             splashScreen.Show();
             Stopwatch timer = new Stopwatch();
+            
+            // hitung time startup
             timer.Start();
-
             base.OnStartup(e);
             Window loginWindow = new AuthWindow();
             timer.Stop();
 
-            int sisaWaktu = DURASI - (int)timer.ElapsedMilliseconds;
-            if (sisaWaktu > 0)
-                Thread.Sleep(sisaWaktu);
+            // hitung waktu reboot splashScreen
+            int waktuReboot = durasiMax - (int)timer.ElapsedMilliseconds;
+            if (waktuReboot > 0)
+                Thread.Sleep(waktuReboot);
 
+            // finalisasi splashScreen
             splashScreen.Close();
             loginWindow.Show();
 
